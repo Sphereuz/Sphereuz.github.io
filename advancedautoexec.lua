8b557b9aeff064f1945a3db4aea73a626f70feea
Spheruz <cooltiktoks431@gmail.com>
2025-01-18 22:09:49 -0600

    Update advancedautoexec.lua

--- advancedautoexec.lua
+++ advancedautoexec.lua
@@ -1,3 +1,4 @@
+--\\ hi
 local sigma = loadstring(game:HttpGet("https://raw.githubusercontent.com/insanedude59/notiflib/main/main"))()
 local executer = identifyexecutor() or "Your executer"
 local ohyourbrotherpassedawayhegets5bigbooms_boom_boom_boom_boom_boom = Instance.new("Sound")
@@ -7,5 +8,5 @@ if _G.PlaySound then
     ohyourbrotherpassedawayhegets5bigbooms_boom_boom_boom_boom_boom:Play()
 end
 if _G.SeeNotification then
-    sigma:Notification(executer.. " is working!", executer.. " has successfully injected!", _G.Font1, _G.Font2, 8)
+    sigma:Notification(executer.. " is working!", executer.. " has successfully injected!", _G.Font1, _G.Font2, _G.NotificationTime)
 end
